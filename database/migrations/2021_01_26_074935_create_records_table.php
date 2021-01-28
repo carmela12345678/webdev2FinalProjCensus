ae26ae7981d3c8f69973891a0c1b435b725cbb82
carmela12345678 <18101172@usc.edu.ph>
2021-01-28 21:42:09 +0800

    added fk user sa record table
    AKO SAAAD GEKAPOY NAAA CHAROOOT

--- database/migrations/2021_01_26_074935_create_records_table.php
+++ database/migrations/2021_01_26_074935_create_records_table.php
@@ -15,6 +15,8 @@ class CreateRecordsTable extends Migration
     {
         Schema::create('records', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->enum('record_status', ['verified','unverified']);
             $table->timestamps();
         });
